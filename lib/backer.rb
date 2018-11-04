04e355d0e68898077133e18e1c11de41a3aca3ab
Rafael Batista <rafaelb24@yahoo.com>
2018-11-04 15:47:25 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,11 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :backed_projects
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
   end
 end
